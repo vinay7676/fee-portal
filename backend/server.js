95bea17e2323201a49c02ffd5c23b9c915a7ba83
vinay <vinaydadwal980@gmail.com>
2026-04-27 21:10:21 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://ephemeral-cannoli-5f809a.netlify.app"],
     credentials: true,
   })
 );
--- frontend/src/Apipath.jsx
+++ frontend/src/Apipath.jsx
@@ -1,1 +1,4 @@
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
+
+
+export const BASE_URL = "https://fee-portal-8xzq.onrender.com";
