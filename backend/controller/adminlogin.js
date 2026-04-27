bce101a64e7230f913645d7384b82bfb0e388d91
vinay7676 <109501195+vinay7676@users.noreply.github.com>
2026-04-27 21:19:38 +0530

    Update adminlogin.js

--- backend/controller/adminlogin.js
+++ backend/controller/adminlogin.js
@@ -67,7 +67,7 @@ export const loginadmin = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
